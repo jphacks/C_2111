fd7cbdb1eabc0ff3a78e2ff387dd6e0b3281600d
coindarw <coindarw@gmail.com>
2021-10-28 00:50:12 +0900

    セキュリティキーをリポジトリから削除

--- django_pjt/settings.py
+++ django_pjt/settings.py
@@ -19,8 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+ifyw5qx_=n*o78g!f=f7gibk_^027(6$j!ihv*g1hub=t44@l'
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

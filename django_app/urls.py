d30224a6d1faa3418673de20621af6c4985e0ee9
tanaso <tennisboy_s@me.com>
2021-10-28 12:07:58 +0000

    forn added

--- django_app/urls.py
+++ django_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'django_app'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('new/', views.new, name='new')
 ]
--- django_pjt/settings.py
+++ django_pjt/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+SECRET_KEY = 'j8lnktr(1_4m7y*f0d%bg_@&@a^)x7rc859#72uaqcy$mvy(6h'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 

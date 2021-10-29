bc9250a2f84479bd54bb6384d26f4e143fe95543
tanaso <tennisboy_s@me.com>
2021-10-29 02:05:16 +0000

    サイト内リンク　完全に理解した

--- django_app/urls.py
+++ django_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'django_app'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('new/', views.new, name='new')
+    path('new/', views.new, name='new'),
+    path('info/', views.info, name='info')
 ]

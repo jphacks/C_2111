054283680271d4030a2e08cad43f4060d7394603
w9zr9b <e1910110@uec.ac.jp>
2021-10-30 12:29:38 +0900

    wordcloud, psn_freq_figを追加

--- graph/psn_freq_graph.py
+++ graph/psn_freq_graph.py
@@ -1,26 +1,31 @@
+import plotly.graph_objects as go
+from collections import Counter, defaultdict
+import pandas as pd
+from goo_lab.goo_apis import Goo
 from itertools import count
+from PIL import Image
+import io
+import base64
 import sys
 sys.path.append("./")
-from goo_lab.goo_apis import Goo
-import pandas as pd
-from collections import Counter, defaultdict
-import plotly.graph_objects as go
 
 app_id = "e8be92a5e7fbf6a4b60bb8ff34cbdbf551e65a626b32090fe095864a7f2565e3"
-g = Goo(app_id=app_id, request_id="record001")#.entity(sentence=sentence, class_filter="PSN")
+# .entity(sentence=sentence, class_filter="PSN")
+g = Goo(app_id=app_id, request_id="record001")
 
-def create_psn_freq_graph(csv_path:str="./user_data/user_data_sampled.csv", text_column_name:str = "tweet"):
-    df = pd.read_csv(csv_path).sample(1000).reindex()
+
+def create_psn_freq_graph_base64(texts):
     l = []
-    for text in df[text_column_name]:
+    for text in texts:
         result = g.entity(sentence=text, class_filter="PSN").json()
         for i in result["ne_list"]:
             l.append(i[0])
-    return Counter(l)
 
-count_result  = create_psn_freq_graph()
-fig = go.Figure()
-fig.add_trace(
-    go.Bar(x = list(count_result.keys()), y = list(count_result.values()))
-)
-fig.show()
+    count_result = Counter(l)
+    fig = go.Figure()
+    fig.add_trace(
+        go.Bar(x=list(count_result.keys()), y=list(count_result.values()))
+    )
+    fig.update_layout(title="特定名詞の出現回数。", xaxis={
+                      'categoryorder': 'total descending'})
+    return fig.to_html(full_html=False, include_plotlyjs=False)

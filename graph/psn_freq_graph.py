f054c5c090d4cfe42a5212b14387a887120df148
w9zr9b <e1910110@uec.ac.jp>
2021-10-30 13:17:47 +0900

    psn_freq_figが表示されないバグを解消

--- graph/psn_freq_graph.py
+++ graph/psn_freq_graph.py
@@ -1,12 +1,15 @@
 import plotly.graph_objects as go
 from collections import Counter, defaultdict
 import pandas as pd
+import numpy as np
 from goo_lab.goo_apis import Goo
 from itertools import count
 from PIL import Image
 import io
 import base64
 import sys
+import matplotlib.pyplot as plt
+import japanize_matplotlib
 sys.path.append("./")
 
 app_id = "e8be92a5e7fbf6a4b60bb8ff34cbdbf551e65a626b32090fe095864a7f2565e3"
@@ -22,10 +25,19 @@ def create_psn_freq_graph_base64(texts):
             l.append(i[0])
 
     count_result = Counter(l)
-    fig = go.Figure()
-    fig.add_trace(
-        go.Bar(x=list(count_result.keys()), y=list(count_result.values()))
-    )
-    fig.update_layout(title="特定名詞の出現回数。", xaxis={
-                      'categoryorder': 'total descending'})
-    return fig.to_html(full_html=False, include_plotlyjs=False)
+
+    fig, ax = plt.subplots()
+    plt.bar(list(count_result.keys()), sorted(list(count_result.values())))
+    plt.xticks(rotation=90)
+    plt.xlabel("固有名詞")
+    plt.ylabel("出現回数")
+    plt.show()
+
+    fig.canvas.draw()
+    im = np.array(fig.canvas.renderer.buffer_rgba())
+
+    img = Image.fromarray(im)
+    buf = io.BytesIO()
+    img.save(buf, format='PNG')
+    base64Img = base64.b64encode(buf.getvalue()).decode().replace("'", "")
+    return base64Img
